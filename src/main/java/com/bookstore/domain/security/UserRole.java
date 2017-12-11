4d92736a3d07682ccf77b531f88fedac15facd41
cse512assignment <yangyuhao830@yahoo.com>
2017-12-10 22:24:58 -0800

    Authority Pojo

---
+++ src/main/java/com/bookstore/domain/security/UserRole.java
@@ -0,0 +1,41 @@
+package com.bookstore.domain.security;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user_role")
+public class UserRole implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long userRoleId;
+	
+	public UserRole(){
+		
+	}
+	
+	public UserRole( User user, Role role){
+		this.user = user;
+		this.role = role;
+	}
+	
+	@ManyToOne( fetch = FetchType.EAGER)
+	@JoinColumn( name = "user_id")
+	private User user;
+	
+	@ManyToOne( fetch = FetchType.EAGER)
+	@JoinColumn( name = "role_id" )
+	private Role role;
+
+}
---
+++ src/main/java/domain/security/copy/Authority.java
@@ -0,0 +1,5 @@
+package domain.security.copy;
+
+public class Authority {
+
+}
